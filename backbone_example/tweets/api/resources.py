88b96a6713bd0539e57eaf88b8c25a30b38c9bdc
Robert Kyle <revspooner@gmail.com>
2012-09-11 10:58:09 +0100

    set created_by on tweet creation, assign read and edit permission to everyone by default

--- backbone_example/tweets/api/resources.py
+++ backbone_example/tweets/api/resources.py
@@ -4,7 +4,16 @@ from tastypie.authorization import Authorization
 from tweets.models import Tweet
 
 class TweetResource(ModelResource):
+    def obj_create(self, bundle, request, **kwargs):
+        if request.user.is_authenticated() and request.user.is_active:
+            user_pk = request.user.pk
+        else:
+            user_pk = None
+        bundle.data['created_by'] = {'pk': user_pk}
+        return super(BlogPostResource, self).obj_create(bundle, request, **kwargs)
+
     class Meta:
+#        exclude = (('created_by'),)
         queryset = Tweet.objects.all()
         authorization = Authorization()
         list_allowed_methods = ['get', 'post']
--- backbone_example/tweets/models.py
+++ backbone_example/tweets/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from guardian.shortcuts import assign
+from django.contirb.auth.models import Group
 
 class Tweet(models.Model):
     message = models.CharField(max_length=140)
     timestamp = models.DateTimeField(auto_now_add=True)
-    # created_by = models.ForeignKey('auth.User')
+    created_by = models.ForeignKey('auth.User', null=True)
     type = models.CharField(max_length=30, null=True, blank=True)
 
     class Meta:
@@ -12,3 +14,23 @@ class Tweet(models.Model):
             ('edit_element', 'Can edit element'),
             ('admin_element', 'Is admin of element'),
         )
+
+    def save(*args, **kwargs):
+        if self.pk is None:
+            created = True
+        else:
+            created = False
+
+        # pre_save
+            
+        super(Tweet, self).save(*args, **kwargs)
+
+        # post_save
+        if created is True:
+            # make it readable and editable by everyone by default
+            everyone = Group.objects.get(name='Everyone')
+            assign('view_element', everyone, self)
+            assign('edit_element', everyone, self)
+            if self.created_by is not None:
+                assign('admin_element', self.created_by, self)
+
