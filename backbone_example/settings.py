ebb9074792650363bbb300866f8ccd8996141f66
shuggiefisher <revspooner@gmail.com>
2012-09-28 18:22:00 +0100

    fix problems with appcache

--- backbone_example/settings.py
+++ backbone_example/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -103,9 +104,24 @@ INSTALLED_APPS = (
     'mptt',
     'pipeline',
     'manifesto',
+    'debug_toolbar',
 
     'base',
     'tweets'
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -178,6 +194,7 @@ PIPELINE_JS = {
             'js/backbone-tastypie.js',
             'js/ICanHaz.min.js',
             'js/appMobile.js',
+            'templates/*.mustache'
         ),
         'output_filename': 'js/mobile.js',
         'manifest': False,
